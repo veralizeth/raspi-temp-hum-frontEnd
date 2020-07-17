7d73ba505c2f162036ce2a15a0cf900e93e17e92
Vera <lizeth.veraro@outlook.com>
2020-07-16 19:13:05 -0700

    css and axios call

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Home from './components/Home';
+import Temperature from './components/Temperature';
+import SideNav from './components/SideNav';
+import BackgroundVideo from './components/BackgroundVideo';
 import './App.css';
+import Humidity from './components/Humidity';
 
 function App() {
+
+  const [temperatureList, setTemperatureList] = useState([]);
+
+  const url = "http://localhost:8080/api"
+  const endPoint = `${url}/devices`
+
+  const getTemperature = (url) => {
+
+    const reformatData = (data) => {
+      console.log(data)
+      return data.map((element) => {
+        console.log(element);
+        return element;
+      });
+    };
+
+    axios.get(url)
+      .then((response) => {
+        console.log(response);
+        const apiTemperatureList = reformatData(response.data);
+        console.log(apiTemperatureList[0].temperatures);
+        setTemperatureList(apiTemperatureList[0].temperatures);
+      })
+      .catch((error) => {
+        console.log(error)
+      });
+  };
+
+  useEffect(() => { getTemperature(endPoint); }, [endPoint]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className='App-body'>
+          <SideNav pageWrapId={"page-wrap"} outerContainerId={"App"} />
+          <BackgroundVideo/>
+          <Home />
+          <Temperature />
+          <Humidity />
+        </div>
       </header>
     </div>
   );
